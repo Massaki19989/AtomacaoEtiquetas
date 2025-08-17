dd19ab89b6463f78ef3558df62719d8f10f3b37a
Massaki19989 <massakidragon@gmail.com>
2025-08-17 12:39:50 -0300

    Api completa

--- app.py
+++ app.py
@@ -6,8 +6,10 @@ import time
 from datetime import datetime
 import os
 from flask import Flask, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/', methods=['POST'])
 def imprimir_excel():
@@ -16,11 +18,15 @@ def imprimir_excel():
         return "Nenhum arquivo Excel enviado", 400
 
     arquivo = request.files['excel']
+    sheet = request.form.get('sheet')
     
     if not arquivo.filename.endswith('.xlsx'):
         return "Arquivo enviado não é um arquivo Excel válido", 400
+    
+    if not sheet:
+        return "Você precisa selecionar uma Planilha"
 
-    df = pd.read_excel(arquivo)
+    df = pd.read_excel(arquivo, sheet_name=sheet)
     date = datetime.now()
     day = f"{date.day:02d}"
     month = f"{date.month:02d}"
@@ -29,7 +35,7 @@ def imprimir_excel():
 
     for index, row in df.iterrows():
         nome = row['Nome']
-        rawPrice = str(row['Valor']).replace(',', '.')
+        rawPrice = str(row['Valor']).replace(',', '.').replace('R$', '')
         preco = float(rawPrice)
         quantidade = math.ceil(int(row['Quantidade']) / 3)  # arredonda para cima
 
@@ -50,11 +56,15 @@ def imprimir_big():
         return "Nenhum arquivo Excel enviado", 400
 
     arquivo = request.files['excel']
+    sheet = request.form.get('sheet')
     
     if not arquivo.filename.endswith('.xlsx'):
         return "Arquivo enviado não é um arquivo Excel válido", 400
 
-    df = pd.read_excel(arquivo)
+    if not sheet:
+        return "Você precisa selecionar uma Planilha"
+
+    df = pd.read_excel(arquivo, sheet_name=sheet)
     date = datetime.now()
     day = f"{date.day:02d}"
     month = f"{date.month:02d}"
@@ -63,7 +73,7 @@ def imprimir_big():
 
     for index, row in df.iterrows():
         nome = row['Nome']
-        rawPrice = str(row['Valor']).replace(',', '.')
+        rawPrice = str(row['Valor']).replace(',', '.').replace('R$', "")
         preco = float(rawPrice)
         quantidade = math.ceil(int(row['Quantidade']) / 2)  # arredonda para cima
 
@@ -73,6 +83,40 @@ def imprimir_big():
         time.sleep(2)
         for i in range(quantidade):
             imprimir_2cols(name_img, path2col)
+        time.sleep(5)
+    
+    return "Impressão concluída com sucesso!", 200
+
+@app.route('/one', methods=['POST'])
+def imprimir_one():
+
+    date = datetime.now()
+    day = f"{date.day:02d}"
+    month = f"{date.month:02d}"
+    year = f"{date.year:04d}"
+    path2col = f"{year}/2 col/{day}-{month}/"
+    path3col = f"{year}/3 col/{day}-{month}/"
+
+    data = request.get_json()
+    nome = data.get('name')
+    rawPrice = str(data.get('price').replace(',', '.').replace('R$', ''))
+    preco = float(rawPrice)
+    quantidade = math.ceil(int(data.get('qtd')) / 2)  # arredonda para cima
+    col = data.get('col')
+
+    if col == '2':
+        os.makedirs(f"./etiquetas/{path2col}", exist_ok=True)  # cria o diretório se não existir
+        name_img = gerar_imagem_2col(nome, preco, path2col)
+        time.sleep(2)
+        for i in range(quantidade):
+            imprimir_2cols(name_img, path2col)
+        time.sleep(5)
+    elif col == '3':
+        os.makedirs(f"./etiquetas/{path3col}", exist_ok=True)  # cria o diretório se não existir
+        name_img = gerar_imagem_3col(nome, preco, path3col)
+        time.sleep(2)
+        for i in range(quantidade):
+            imprimir_imagem(name_img, path3col)
         time.sleep(5)
     
     return "Impressão concluída com sucesso!", 200
